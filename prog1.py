390d7354a845c4f1c81f302f489c5f1bf6679843
Pentest Diaries <34878698+pentest-diaries@users.noreply.github.com>
2021-02-17 08:26:03 +0530

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,5 @@
+print("enter the author name:")
+c1 = input()
+library = {'Author1':'Book1','Author2':'Book2','Author3':'Book3','Author4':'Book4'}
+print(" the value:", c1)
+print(c1,"book belongs to this author:", library[c1])
